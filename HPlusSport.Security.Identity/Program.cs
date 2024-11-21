a7d49ae7004806ae044188d645d49f0960071afe
abhay <abhay@DESKTOP-LFN1RVS>
2024-11-21 18:56:14 -0500

    added security for the API and finished chapter 3

--- HPlusSport.Security.Identity/Program.cs
+++ HPlusSport.Security.Identity/Program.cs
@@ -1,10 +1,14 @@
 using HPlusSport.Security.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -12,6 +16,29 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.ConfigureApplicationCookie(o =>
+{
+    o.Events = new CookieAuthenticationEvents()
+    {
+        OnRedirectToLogin = (ctx) =>
+        {
+            if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+            {
+                ctx.Response.StatusCode = 401;
+            }
+            return Task.CompletedTask;
+        },
+        OnRedirectToAccessDenied = (ctx) =>
+        {
+            if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+            {
+                ctx.Response.StatusCode = 403;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -36,5 +63,14 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapSwagger();
+
+app.MapGet("/api/hello", (HttpContext HttpContext) =>
+{
+    return $"Hello {HttpContext.User?.Identity?.Name}";
+})
+    .WithOpenApi()
+    .RequireAuthorization();
 
 app.Run();
